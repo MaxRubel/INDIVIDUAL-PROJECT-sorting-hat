975aba9a12876019b2d9295c6d7328a6cbbde449
MaxRubel <maxrubel1@gmail.com>
2023-10-28 14:08:32 -0500

    refactored hat to form function

--- main.js
+++ main.js
@@ -18,28 +18,44 @@ function renderToDom(){
 
 }
 
-const sortingHat=document.querySelector('#sortingHat');
+
 const studentArray=document.querySelector('#studentArray');
-sortingHat.addEventListener('click', ()=>{
-  sortingHat.innerHTML=userForm;
-  console.log('hey');
-})
 
-let sortingHatInner='<img class="sortingHatPhoto" src="/images/sortingHat.png" height="300px" width="300px"></div>';
+{/* <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div> */}
 
-const userForm=
-`<form>
-<div class="mb-3">
-  <label for="studentName" class="form-label">Student Name</label>
-  <input type="name" class="form-control" id="studentName" aria-describedby="emailHelp">
-  <div id="emailHelp" class="form-text">Please enter a lil' name for your student.</div>
-</div>
-<button type="submit" id="submit" class="btn btn-primary">Submit</button>
-</form>`;
+function hatToForm(){
+  const sortingHat=document.querySelector('#sortingHat');
+  let sortingHatImage='<img class="sortingHatPhoto" src="/images/sortingHat.png" height="300px" width="300px"></div>';
+  
+  const userForm=
+    `<form>
+      <div class="mb-3">
+        <label for="studentName" class="form-label">Student Name</label>
+        <input type="name" class="form-control" id="studentName" aria-describedby="emailHelp">
+        <div id="emailHelp" class="form-text">Please enter a lil' name for your student.</div>
+      </div>
+      <button type="submit" id="submit" class="btn btn-primary">Submit</button>
+    </form>`;
 
-sortingHat.innerHTML=sortingHatInner;
+  sortingHat.innerHTML=sortingHatImage;
 
-form.addEventListener('submit', (event)=> {
-  event.preventDefault();
-  console.log("hey")
-})
+  sortingHat.addEventListener('click', ()=>{
+    sortingHat.innerHTML=userForm;
+    console.log('hey');
+  })
+
+  form.addEventListener('submit', (event)=> {
+    event.preventDefault();
+    sortingHat.innerHTML=userForm;
+    console.log("hey")
+  })
+}
+
+hatToForm();
