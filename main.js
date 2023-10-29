f7e130a7cc894f208fb0405125c0ec7ec4a66a75
MaxRubel <maxrubel1@gmail.com>
2023-10-28 20:21:20 -0500

    Still need to make the expel function work

--- main.js
+++ main.js
@@ -2,51 +2,65 @@ const students = [
   {
     name: "max",
     id: 0,
-    house: "hufflepuff",
+    house: "Gryffindor",
+    enrolled: true,
+  },
+  {
+    name: "jake",
+    id: 0,
+    house: "Slytherin",
+    enrolled: true,
+  },
+  {
+    name: "pete",
+    id: 0,
+    house: "Ravenclaw",
     enrolled: true,
   },
   {
     name: "john",
     id: 1,
-    house: "Gryff",
+    house: "Hufflepuff",
     enrolled: true,
   },
 ];
 
-// const studentArray = document.querySelector("#studentArray");
+const expelled = [];
 
 //RENDER ENROLLED STUDENS
 function renderStudentCards(array) {
   let domString = "";
   for (student of array) {
-    domString += ` 
-      <div class="studentCard" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">${student.name}</h5>
-          <p class="card-text">${student.house}</p>
-          <a href="#" class="btn btn-primary" id="expel--${student.id}">Expel!</a>
-        </div>
-      </div> `;
+    domString += `<table class="table table-dark table-striped" style="width: 600px">
+    <thead>
+      <tr>
+        <th scope="col" width="30%">${student.name}</th>
+        <th scope="col" width="60%">${student.house}</th>
+        <th scope="col"><button type="button" class="btn btn-danger" id="expel--${student.id}">Expel!</button></th>
+      </tr>
+    </thead>
+    <tbody>
+    `;
   }
   document.querySelector("#studentArray").innerHTML = domString;
 }
 
-function renderExpelled(array) {
-  let domString = "";
-  for (student of array) {
-    domString += ` 
-      <div class="studentCard" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">${student.name}</h5>
-          <p class="card-text">${student.house}</p>
-          <a href="#" class="btn btn-primary" id="expel--${student.id}">Expel!</a>
-        </div>
-      </div> `;
-  }
-  document.querySelector("#expelled").innerHTML = domString;
-}
+// function renderExpelled() {
+//   let domString = "";
+//   for (student of expelled) {
+//     domString += `<table class="table table-dark table-striped" style="width: 600px">
+//     <thead>
+//       <tr>
+//         <th scope="col" width="30%">${student.name}</th>
+//         <th scope="col" width="60%">${student.house}</th>
+//         <th scope="col"><button type="button" class="btn btn-danger" id="expel--${student.id}">Delete</button></th>
+//       </tr>
+//     </thead>
+//     <tbody>
+//     `;
+//   }
+//   document.querySelector("#expelled").innerHTML = domString;
+// }
 
 function event() {
   const sortingHat = document.querySelector("#sortingHat");
@@ -54,8 +68,7 @@ function event() {
 
   //SHOW ALL
   renderStudentCards(students);
-  // renderExpelled(expelled);
-  
+
   //CLICK ON HAT TO CREATE FORM
   sortingHat.addEventListener("click", () => {
     sortingHat.style.display = "none";
@@ -76,21 +89,7 @@ function event() {
     renderStudentCards(students);
   });
 
-  //EXPELL STUDENT
-  studentArray.addEventListener("click", (event) => {
-    const expelled = [];
-    if (event.target.id.includes("expel")) {
-      const [, id] = event.target.id.split("--");
-      const index = students.findIndex((obj) => obj.id === Number(id));
-      expelled.push(students[id]);
-      students.splice(index, 1);
-      renderStudentCards(students);
-      renderExpelled(expelled);
-    }
-  });
-}
-
-//ASSIGN THE HOUSE
+  //ASSIGN THE HOUSE
 function assignHouse() {
   let randomInt = Math.floor(Math.random() * 12);
   if (randomInt >= 0 && randomInt < 3) {
@@ -104,6 +103,50 @@ function assignHouse() {
   }
 }
 
+  //FILTER FUNCTION
+  const filter = (house) => {
+    let houseArray = [];
+    for (student of students) {
+      if (student.house === house) {
+        console.log(student);
+        houseArray.push(student);
+      }
+    }
+    renderStudentCards(houseArray);
+  };
+  
+  //FILTER BUTTON EVENT LISTENERS
+  document.querySelector("#gryffindor").addEventListener("click", () => {
+    filter("Gryffindor");
+  });
+  
+  document.querySelector("#slytherin").addEventListener("click", () => {
+    filter("Slytherin");
+  });
+  
+  document.querySelector("#ravenclaw").addEventListener("click", () => {
+    filter("Ravenclaw");
+  });
+  
+  document.querySelector("#hufflepuff").addEventListener("click", () => {
+    filter("Hufflepuff");
+  });
 
+  document.querySelector("#allHouses").addEventListener("click", () => {
+    renderStudentCards(students);
+  });
+
+  //EXPELL STUDENT FUNCTION
+  studentArray.addEventListener("click", (event) => {
+    if (event.target.id.includes("expel")) {
+      const [, id] = event.target.id.split("--");
+      const index = students.findIndex((obj) => obj.id === Number(id));
+      // expelled.push(students[id]);
+      students.splice(index, 1);
+      renderStudentCards(students);
+      // renderExpelled();
+    }
+  });
+}
 
 event();
