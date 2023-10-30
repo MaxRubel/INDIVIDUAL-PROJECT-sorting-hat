9bc363127f710205450da222dece2ed08a677e6c
MaxRubel <maxrubel1@gmail.com>
2023-10-30 02:06:08 -0500

    everything is working properly. MVP reached

--- main.js
+++ main.js
@@ -1,150 +1,171 @@
-const students = [
-  {
-    name: "max",
-    id: 0,
-    house: "Gryffindor",
-    enrolled: true,
-  },
-  {
-    name: "jake",
-    id: 0,
-    house: "Slytherin",
-    enrolled: true,
-  },
-  {
-    name: "pete",
-    id: 0,
-    house: "Ravenclaw",
-    enrolled: true,
-  },
-  {
-    name: "john",
-    id: 1,
-    house: "Hufflepuff",
-    enrolled: true,
-  },
-];
-
-const expelled = [];
-
-//RENDER ENROLLED STUDENS
-function renderStudentCards(array) {
-  let domString = "";
-  for (student of array) {
-    domString += `<table class="table table-dark table-striped" style="width: 600px">
-    <thead>
-      <tr>
-        <th scope="col" width="30%">${student.name}</th>
-        <th scope="col" width="60%">${student.house}</th>
-        <th scope="col"><button type="button" class="btn btn-danger" id="expel--${student.id}">Expel!</button></th>
-      </tr>
-    </thead>
-    <tbody>
-    `;
-  }
-  document.querySelector("#studentArray").innerHTML = domString;
-}
-
-// function renderExpelled() {
-//   let domString = "";
-//   for (student of expelled) {
-//     domString += `<table class="table table-dark table-striped" style="width: 600px">
-//     <thead>
-//       <tr>
-//         <th scope="col" width="30%">${student.name}</th>
-//         <th scope="col" width="60%">${student.house}</th>
-//         <th scope="col"><button type="button" class="btn btn-danger" id="expel--${student.id}">Delete</button></th>
-//       </tr>
-//     </thead>
-//     <tbody>
-//     `;
-//   }
-//   document.querySelector("#expelled").innerHTML = domString;
-// }
-
+//ALL FUNCTIONS
 function event() {
-  const sortingHat = document.querySelector("#sortingHat");
-  const inputForm = document.querySelector("#inputForm");
+  
+  //MAIN ARRAY DATA
+  let students = [
+    {
+      name: "Max",
+      id: 0,
+      house: "Gryffindor",
+      status: "enrolled",
+    },
+    {
+      name: "John",
+      id: 1,
+      house: "Slytherin",
+      status: "enrolled",
+    },
+    {
+      name: "Tatianna",
+      id: 2,
+      house: "Ravenclaw",
+      status: "enrolled",
+    },
+    {
+      name: "Jesse",
+      id: 3,
+      house: "Hufflepuff",
+      status: "enrolled",
+    },
+  ];
+
+  //RENDER STUDENTS
+  function renderStudentCards(array) {
+    let domString = "";
+    let domStringEx = "";
+    for (student of array) {
+      if (student.status === "enrolled") {
+        domString += `<table class="table table-dark table-striped" style="width: 600px">
+      <thead>
+        <tr style="height: 8px">
+          <th scope="col" width="30%">${student.name}</th>
+          <th scope="col" width="60%">${student.house}</th>
+          <th scope="col" id="expell"><button type="button" class="btn btn-danger" id="expel--${student.id}">Expel!</button></th>
+        </tr>
+      </thead>
+      <tbody>
+      `;
+      } else if (student.status === "expelled") {
+        domStringEx += `<table class="table table-dark table-striped" style="width: 600px">
+        <thead>
+          <tr style="height: 10px;">
+            <th scope="col" width="30%" style="background-color: red">${student.name}</th>
+            <th scope="col" width="51%">${student.house}</th>
+            <th scope="col" class="reEnroll" 
+            style="
+            display: flex;
+            justify-content: right"
+            padding: 0;
+            >
+            <button type="button" class="btn btn-danger" id="reEnroll--${student.id}">Re-enroll</button></th>
+          </tr>
+        </thead>
+        <tbody>
+        `;
+      }
+      document.getElementById("expelled").innerHTML = domStringEx;
+      document.getElementById("studentArray").innerHTML = domString;
+    }
+  }
 
   //SHOW ALL
   renderStudentCards(students);
 
   //CLICK ON HAT TO CREATE FORM
+  const sortingHat = document.getElementById("sortingHat");
   sortingHat.addEventListener("click", () => {
     sortingHat.style.display = "none";
-    inputForm.style.display = "block";
+    inputForm.style.display = "flex";
   });
 
   //SUBMIT A NAME. CREATE A HOUSE. ADD TO ARRAY. RENDER
-  inputForm.addEventListener("submit", (event) => {
+  const form = document.getElementById("submitStudent");
+  nameInput = document.getElementById("studentName");
+  form.addEventListener("submit", (event) => {
     event.preventDefault();
-    let newStudent = {
-      name: document.querySelector("#studentName").value,
-      id: students.length + 1,
-      house: assignHouse(),
-    };
-
-    console.log(newStudent);
-    students.push(newStudent);
-    renderStudentCards(students);
+    if (nameInput.value.length === 0) {
+      document.getElementById("error").style =
+        "display:block; text-align: center";
+      return;
+    } else {
+      const newStudent = {
+        name: document.getElementById("studentName").value,
+        id: students.length + 1,
+        house: assignHouse(),
+        status: "enrolled",
+      };
+      students.unshift(newStudent);
+      document.getElementById("error").style =
+        "display:none; text-align: center";
+      form.reset();
+      renderStudentCards(students);
+    }
   });
 
   //ASSIGN THE HOUSE
-function assignHouse() {
-  let randomInt = Math.floor(Math.random() * 12);
-  if (randomInt >= 0 && randomInt < 3) {
-    return "Gryffindor";
-  } else if (randomInt >= 3 && randomInt < 6) {
-    return "Slythern";
-  } else if (randomInt >= 6 && randomInt < 9) {
-    return "Ravenclaw";
-  } else if (randomInt >= 9 && randomInt <= 11) {
-    return "HufflePuff";
+  function assignHouse() {
+    let randomInt = Math.floor(Math.random() * 12);
+    if (randomInt >= 0 && randomInt < 3) {
+      return "Gryffindor";
+    } else if (randomInt >= 3 && randomInt < 6) {
+      return "Slytherin";
+    } else if (randomInt >= 6 && randomInt < 9) {
+      return "Ravenclaw";
+    } else if (randomInt >= 9 && randomInt <= 11) {
+      return "Hufflepuff";
+    }
   }
-}
 
   //FILTER FUNCTION
   const filter = (house) => {
     let houseArray = [];
     for (student of students) {
       if (student.house === house) {
-        console.log(student);
         houseArray.push(student);
       }
     }
+    document.getElementById("studentArray").innerHTML = "";
+    document.getElementById("expelled").innerHTML = "";
     renderStudentCards(houseArray);
   };
-  
+
   //FILTER BUTTON EVENT LISTENERS
   document.querySelector("#gryffindor").addEventListener("click", () => {
     filter("Gryffindor");
   });
-  
   document.querySelector("#slytherin").addEventListener("click", () => {
     filter("Slytherin");
   });
-  
   document.querySelector("#ravenclaw").addEventListener("click", () => {
     filter("Ravenclaw");
   });
-  
   document.querySelector("#hufflepuff").addEventListener("click", () => {
     filter("Hufflepuff");
   });
-
   document.querySelector("#allHouses").addEventListener("click", () => {
     renderStudentCards(students);
   });
 
-  //EXPELL STUDENT FUNCTION
-  studentArray.addEventListener("click", (event) => {
+  //EXPELL FUNCTION
+  addEventListener("click", (event) => {
     if (event.target.id.includes("expel")) {
       const [, id] = event.target.id.split("--");
       const index = students.findIndex((obj) => obj.id === Number(id));
-      // expelled.push(students[id]);
-      students.splice(index, 1);
+      students[index].status = "expelled";
+      console.log(students[index]);
       renderStudentCards(students);
-      // renderExpelled();
+      return students;
+    }
+  });
+
+  //RE-ENROLL FUNCTION
+  addEventListener("click", (event) => {
+    if (event.target.id.includes("reEnroll")) {
+      const [, id] = event.target.id.split("--");
+      const index = students.findIndex((obj) => obj.id === Number(id));
+      students[index].status = "enrolled";
+      // console.log(students[index].name);
+      renderStudentCards(students);
     }
   });
 }
